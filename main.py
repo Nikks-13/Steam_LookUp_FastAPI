69bf9809a296cd1bfdbba93147817e3bb66c6070
NIkhil Kumar Singh <nks130501@gmail.com>
2023-12-31 14:00:51 +0530

    added origin main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:3000",
-    "https://nixx-discord-lookup.netlify.app",
+    "https://nixx-steam-lookup.netlify.app",
     # Add any other allowed origins
 ]
 
